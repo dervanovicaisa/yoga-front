f424b09f9aaf4cc84d736e67ad61e1f6840c6388
Aiša Dervanović <dervanovicaisa@gmail.com>
2024-02-17 17:17:55 +0100

    responsive

--- src/services/Services.js
+++ src/services/Services.js
@@ -7,18 +7,18 @@ import "../services/services.css";
 function Services() {
   return (
     <Row id="services" className="py-4">
-      <Col>
+      <Col lg={8} md={12}>
         <Row className="gap-4">
-          <Col>
+          <Col md={12}>
             <Image src={ArrowLeft} />
             <Image src={ArrowRight} className="mx-5" />
           </Col>
-          <Col className="pt-3 px-5">
-            <Image src={YogaService} />
+          <Col className="pt-3 px-5" md={12}>
+            <Image src={YogaService} className="w-100" />
           </Col>
         </Row>
       </Col>
-      <Col className="text-service">
+      <Col lg={4} md={12} className="text-service mt-4 mt-lg-0">
         <h1 className="text-purple text-bold w-50 text-wrap">Our Services</h1>
         <div className="bg-light-purple ptb-5px my-4" />
         <p className="text-purple  w-75">
